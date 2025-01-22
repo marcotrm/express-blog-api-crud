e2c4ab87c3dcd048e29122e954338942ca9773d0
marco torremacco <mtorremacco@gmail.comm>
2025-01-22 16:06:36 +0100

    feat add middleware (validation)

--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController')
+const validationParamId = require('../middlewares/validationParamId')
 
 // index
   router.get('/', postsController.index)
 
 // show
-  router.get('/:id', postsController.show) 
+  router.get('/:id', validationParamId, postsController.show) 
 
 // store
   router.post('/', postsController.store)
 
 // update
-  router.put('/:id', postsController.update)
+  router.put("/:id", validationParamId, postsController.update);
 
 // modify
-  router.patch('/:id', postsController.modify)
+  router.patch("/:id", validationParamId, postsController.modify);
  
 // destroy
-  router.delete('/:id', postsController.destroy)
+  router.delete("/:id", validationParamId, postsController.destroy);
 
 module.exports = router;
