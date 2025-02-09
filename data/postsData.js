c20d431fb583a672d64419446f05532bee0cc6e8
marco torremacco <mtorremacco@gmail.comm>
2025-02-09 17:11:51 +0100

    feat added images of pizza

--- data/postsData.js
+++ data/postsData.js
@@ -4,35 +4,36 @@ const posts = [
     name: "Margherita",
     ingredients: "Pomodoro, Mozzarella, Basilico",
     price: "€7.00",
-    image: "margherita.jpg",
+    image: "/img/margherita.jpg", // ✅ Percorso corretto
   },
   {
     id: 2,
     name: "Diavola",
     ingredients: "Pomodoro, Mozzarella, Salame Piccante",
     price: "€8.50",
-    image: "diavola.jpg",
+    image: "/img/diavola.jpg",
   },
   {
     id: 3,
     name: "Quattro Formaggi",
     ingredients: "Mozzarella, Gorgonzola, Fontina, Parmigiano",
     price: "€9.00",
-    image: "quattroformaggi.jpg",
+    image: "/img/quattro-formaggi.jpg",
   },
   {
     id: 4,
     name: "Capricciosa",
     ingredients: "Pomodoro, Mozzarella, Funghi, Prosciutto, Olive",
     price: "€9.50",
-    image: "capricciosa.jpg",
+    image: "/img/capricciosa.jpg",
   },
   {
     id: 5,
     name: "Boscaiola",
     ingredients: "Mozzarella, Funghi, Salsiccia",
     price: "€9.00",
-    image: "boscaiola.jpg",
+    image: "/img/boscaiola.jpg",
   },
 ];
-module.exports = posts;
+
+export default posts;
